73b322099092825d772a8265682d2584c136390d
anushkajagtap1922-lang <anushkajagtap1922@gmail.com>
2026-09-21 13:54:50 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int length,breadth,area;
+clrscr();
+
+printf("Enter length and breadth:");
+scanf("%d%d",&length,&breadth);
+
+area=length*breadth;
+
+printf("Area = %d",area);
+
+getch();
+}
